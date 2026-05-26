150fc0bc427f9141917213aac7d4996f5ff95047
WaiYanMoeLwin <waiyanmoelwin1710@gmail.com>
2026-05-26 15:25:27 +0700

    added difficulty filters

--- src/api/video.ts
+++ src/api/video.ts
@@ -4,3 +4,8 @@ export const getFeedVideos = async (search='') => {
     const response = await apiClient.get("/feed", { params: { search } });
     return response.data;
 };
+
+export const getVideosByDifficulty = async (difficulty_level: string) => {
+    const response = await apiClient.get("/feed/difficulty/" + difficulty_level);
+    return response.data;
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getFeedVideos } from '../api/video';
+import { getFeedVideos, getVideosByDifficulty } from '../api/video';
 import { SearchBar } from '../components/common/SearchBar';
 import { Feed } from '../components/home/Feed';
 import type { Video } from '../types/user';
@@ -21,11 +21,21 @@ function HomePage() {
         const videos = await getFeedVideos(query);
         setVideos(videos);
     };
+
+    const handleFilter = async (level: string) => {
+        const videos = await getVideosByDifficulty(level);
+        setVideos(videos);
+    }
     
     return (
         <div className="bg-[#0a1628] h-screen flex flex-col p-4 mx-6 text-white">
             <h1 className='text-3xl text-start w-full font-bold mb-8'>Your Feed</h1>
             <SearchBar onSearch={handleSearch} />
+            <div className='flex flex-row gap-2 mt-4'>
+                <button className='rounded-2xl m-2 px-2 py-1 bg-teal-800 text-white hover:bg-teal-600' onClick={() => handleFilter('beginner')}>Beginner</button>
+                <button className='rounded-2xl m-2 px-2 py-1 bg-teal-800 text-white hover:bg-teal-600' onClick={() => handleFilter('intermediate')}>Intermediate</button>
+                <button className='rounded-2xl m-2 px-2 py-1 bg-teal-800 text-white hover:bg-teal-600' onClick={() => handleFilter('advanced')}>Advanced</button>
+            </div>
             <Feed videos={videos} />
 
         </div>
